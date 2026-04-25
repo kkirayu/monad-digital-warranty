62ab0abb07561e135023dc839baede7edbc62fa5
kkirayu <kukicha76@gmail.com>
2026-04-25 17:45:04 +0700

    last edit

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    target: 'esnext', // <-- TAMBAHKAN INI (Sangat ampuh untuk error Web3)
+    minify: false     // <-- (OPSIONAL UNTUK HACKATHON) Matikan minifikasi sementara biar kodenya gak rusak saat di-deploy
+  }
 })
